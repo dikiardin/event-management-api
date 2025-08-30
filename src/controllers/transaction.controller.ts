26e3328eabb41d4dc53c35f4d1da5ff54738a450
nicholas-0101 <nsvertian@gmail.com>
2025-08-30 10:57:15 +0700

    merge with diki's part (#20)

---
+++ src/controllers/transaction.controller.ts
@@ -0,0 +1,136 @@
+import { Request, Response, NextFunction } from "express";
+import { TransactionService } from "../service/transaction.service";
+
+export class TransactionController {
+  // create transaction
+  public async createTransaction(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const userId = res.locals.decrypt?.id;
+      if (!userId) throw new Error("Unauthorized");
+
+      const { tickets, pointId, couponId, voucherId } = req.body;
+
+      const transaction = await TransactionService.createTransactionService(
+        userId,
+        tickets,
+        couponId,
+        voucherId,
+        pointId
+      );
+      res.status(201).json({
+        success: true,
+        message: "Transaction created succesfully",
+        transaction,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // upload payment proof
+  public async uploadPaymentProof(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const userId = res.locals.decrypt?.id;
+      if (!userId) throw new Error("Unauthorized");
+
+      const { id } = req.params;
+      if (!req.file) throw new Error("No payment proof file uploaded");
+
+      // call service
+      const transaction = await TransactionService.uploadPaymentProofService(
+        Number(id),
+        req.file,
+        userId
+      );
+
+      if (transaction.user_id !== userId) {
+        throw new Error("Unauthorized: cannot modify other user's transaction");
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Payment proof uploaded successfully",
+        transaction,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // cancel transaction
+  public async cancelTransaction(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const userId = res.locals.decrypt?.id;
+      if (!userId) throw new Error("Unauthorized");
+
+      const { id } = req.params;
+
+      // check ownership inside service
+      const transaction = await TransactionService.cancelTransactionService(
+        Number(id)
+      );
+      if (transaction.user_id !== userId)
+        throw new Error("Unauthorized: cannot cancel other user's transaction");
+
+      res
+        .status(200)
+        .json({ success: true, message: "Transaction calcelled", transaction });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // get transactions by user id
+  public async getTransactionsByUserId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { userId } = req.params;
+      const transactions =
+        await TransactionService.getTransactionsByUserIdService(Number(userId));
+      return res.status(200).json({
+        success: true,
+        message: "transaction by user id catched succesfully",
+        transactions,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // get transactions by event id
+  public async getTransactionsByEventId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { eventId } = req.params;
+      const transactions =
+        await TransactionService.getTransactionsByEventIdService(
+          Number(eventId)
+        );
+      return res.status(200).json({
+        success: true,
+        message: "transaction by event id catched succesfully",
+        transactions,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
